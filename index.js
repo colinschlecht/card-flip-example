f963b4f42be3f6152532a6a4f1f8861e4fa212d5
Colin Schlecht <schlechtcolin@yahoo.com>
2021-03-14 22:14:52 -0700

    adds hover examples

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let cards = document.querySelectorAll('.card')
 
 
 for(let card of cards){
-       card.addEventListener('click', function (){
-        card.classList.toggle('flipped')
+    // card.addEventListener('mouseover', function (){
+    card.addEventListener('click', function (){
+    card.classList.toggle('flipped')
     })
 }
